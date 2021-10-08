01260d96d7b4f9b6a8ede5e0968fa852c726c201
Tommy Volk <tvolk131@gmail.com>
2021-10-08 15:07:41 +0000

    Cleanup a few widgets

--- client/src/taprootCountdownWidget.tsx
+++ client/src/taprootCountdownWidget.tsx
@@ -42,10 +42,12 @@ export const TaprootCountdownWidget = (props: TaprootCountdownWidgetProps) => {
     <div style={{padding: '10px'}}>
       <Paper style={{height: '400px', width: '400px'}}>
         {taprootHasActivated ? (
-          <Typography variant={'h5'} style={{padding: '10px', textAlign: 'center'}}>Taproot has activated!</Typography>
+          <Typography variant={'h4'} style={{padding: '10px', textAlign: 'center'}}>Taproot has activated!</Typography>
         ) : (
-          <div>
-            <Typography variant={'h5'} style={{padding: '10px', textAlign: 'center'}}>Taproot will activate in {taprootBlockHeight - props.blockHeight} blocks</Typography>
+          <div style={{padding: '75px 0'}}>
+            <Typography variant={'h4'} style={{padding: '10px', textAlign: 'center'}}>Taproot Activation</Typography>
+            <Typography variant={'h6'} style={{padding: '10px', textAlign: 'center'}}>will occur in</Typography>
+            <Typography variant={'h2'} style={{padding: '10px', textAlign: 'center'}}>{taprootBlockHeight - props.blockHeight} blocks</Typography>
             <Typography style={{padding: '10px', textAlign: 'center'}}>Approximately {getDurationEstimateFromBlockCount(blocksUntilTaproot)}</Typography>
           </div>
         )}
