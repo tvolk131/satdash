31ff78b82ec5a4fbaf42c355cffb894d9487e098
Tommy Volk <tvolk131@gmail.com>
2022-09-29 18:27:07 +0000

    Optimize BTC price history struct

--- server/src/bpi/btc_csv.rs
+++ server/src/bpi/btc_csv.rs
@@ -2,36 +2,37 @@ use serde::Deserialize;
 use std::collections::HashMap;
 
 pub struct BTCPriceHistory {
-    entries_by_date: HashMap<String, BTCPriceCSVEntry>,
+    price_by_date: HashMap<(i32, i32, i32), f64>,
 }
 
 impl BTCPriceHistory {
     pub fn new_from_reader(reader: impl std::io::Read) -> Result<Self, csv::Error> {
-        let mut entries_by_date = HashMap::new();
+        let mut price_by_date = HashMap::new();
 
         let mut rdr = csv::Reader::from_reader(reader);
 
         for result in rdr.deserialize() {
             let entry: BTCPriceCSVEntry = result?;
-            entries_by_date.insert(entry.date.clone(), entry);
+            let mut date_parts_iter = entry.date.split("-");
+            let year = date_parts_iter.next().unwrap().parse::<i32>().unwrap();
+            let month = date_parts_iter.next().unwrap().parse::<i32>().unwrap();
+            let day = date_parts_iter.next().unwrap().parse::<i32>().unwrap();
+            price_by_date.insert((year, month, day), entry.open.parse::<f64>().unwrap());
         }
 
-        Ok(Self { entries_by_date })
+        Ok(Self { price_by_date })
     }
 
     pub fn get_average_price_for_month(&self, year: i32, month: i32) -> Option<f64> {
         let mut price_sum = 0.0;
         let mut price_count = 0;
 
-        let month_string = format!("{:0>2}", month);
-
         for day in 1..31 {
-            let day_string = format!("{:0>2}", day);
-            if let Some(entry) = self
-                .entries_by_date
-                .get(&format!("{}-{}-{}", year, month_string, day_string))
+            if let Some(price) = self
+                .price_by_date
+                .get(&(year, month, day))
             {
-                price_sum += entry.open.parse::<f64>().unwrap();
+                price_sum += price;
                 price_count += 1;
             }
         }
